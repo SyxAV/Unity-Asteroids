f986a6c6ac85aa24394528b9fd056fb884cab059
Riccardo <riccardospadaro58@gmail.com>
2023-03-09 19:41:24 +0100

    Spaceship movements

--- Asteroids/Assets/_Scripts/Player.cs
+++ Asteroids/Assets/_Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 moveDir;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir = new Vector3(0, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveDir = new Vector3(0, -1, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveDir = new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveDir = new Vector3(1, 0, 0);
+        }
+        else
+        {
+            moveDir = Vector3.zero;
+        }
+
+        float moveSpeed = 3 * Time.deltaTime;
+        transform.position += moveSpeed * moveDir;
     }
 }
